92248f54299b49b4dd5dcb665cc530613ef30268
haha <2524742620@qq.com>
2016-04-01 15:05:48 +0800

    B创建了一个personB类

---
+++ git/git/PersonB.h
@@ -0,0 +1,13 @@
+//
+//  PersonB.h
+//  git
+//
+//  Created by Qianfeng on 16/4/1.
+//  Copyright © 2016年 周方超. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonB : NSObject
+
+@end
---
+++ git/git/PersonB.m
@@ -0,0 +1,13 @@
+//
+//  PersonB.m
+//  git
+//
+//  Created by Qianfeng on 16/4/1.
+//  Copyright © 2016年 周方超. All rights reserved.
+//
+
+#import "PersonB.h"
+
+@implementation PersonB
+
+@end
